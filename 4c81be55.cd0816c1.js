340b6069ff147966e8d2f37e7d6c52445d1dc1d8
Sumit Agrawal <gophersumit@outlook.com>
2020-02-25 09:48:23 +0530

    Deploy website version based on HEAD

---
+++ 4c81be55.cd0816c1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{102:function(o){o.exports=JSON.parse('{"allTagsPath":"/yomango.github.io/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/yomango.github.io/blog/tags/hello"}')}}]);
---
+++ eaa24137.77b38fc0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{117:function(o){o.exports=JSON.parse('{"allTagsPath":"/yomango.github.io/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/yomango.github.io/blog/tags/facebook"}')}}]);
